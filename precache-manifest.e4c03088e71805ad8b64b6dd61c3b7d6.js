883765ad64fbfd78abcc2d5361f4af23419f06c4
text5 <text5by@gmail.com>
2020-02-11 19:59:01 +0300

    Updates

---
+++ precache-manifest.e4c03088e71805ad8b64b6dd61c3b7d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a495132ed47ac0992e750cedbcfb91fa",
+    "url": "/songbirds/index.html"
+  },
+  {
+    "revision": "d8ffc2b06bb8f625803b",
+    "url": "/songbirds/static/css/main.10af5697.chunk.css"
+  },
+  {
+    "revision": "02436796af208cc7419e",
+    "url": "/songbirds/static/js/2.12ebb6db.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/songbirds/static/js/2.12ebb6db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8ffc2b06bb8f625803b",
+    "url": "/songbirds/static/js/main.5944859f.chunk.js"
+  },
+  {
+    "revision": "60fdff808aa4f9459f3e",
+    "url": "/songbirds/static/js/runtime-main.55acf412.js"
+  }
+]);
